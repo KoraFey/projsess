098b2cf06e697f9eac9532c2f22c3f27c4da94aa
Alexis00Roy <156560114+Alexis00Roy@users.noreply.github.com>
2024-03-18 12:09:14 -0400

    Create SQL_Create.sql

---
+++ Documentation/SQL_Create.sql
@@ -0,0 +1,31 @@
+--create table user
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    url_pfp VARCHAR(255)
+    type_usager ENUM('regulier', 'admin') NOT NULL DEFAULT 'regulier';
+);
+
+
+--creation de la table settings
+CREATE TABLE settings(
+    user_id INT,
+    dark_mode BOOLEAN NOT NULL DEFAULT 0,
+    notification BOOLEAN NOT NULL DEFAULT 0,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+);
+
+
+--table block list
+CREATE TABLE Block_List(
+    id int PRIMARY KEY,
+    user_id int NOT NULL,
+    blocked_id int NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (blocked_id) REFERENCES users(id)
+
+);
