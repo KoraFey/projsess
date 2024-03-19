f82779af77dbd30353030fb57731f4e606f71bf6
Alexis00Roy <156560114+Alexis00Roy@users.noreply.github.com>
2024-03-19 09:59:30 -0400

    Update login.php
    test

--- login.php
+++ login.php
@@ -7,7 +7,8 @@ $error ='';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = $_POST['username'];
   $password = $_POST['password'];
-
+  echo $username;
+  echo $password;
   $stmt = $pdo->prepare('SELECT * FROM users WHERE username = ?');
   $stmt->execute([$username]);
   $user = $stmt->fetch();
