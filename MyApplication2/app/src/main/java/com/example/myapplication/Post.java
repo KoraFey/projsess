43e1876065a5bc5460fb78a511d0148a2f570366
Alexis00Roy <156560114+Alexis00Roy@users.noreply.github.com>
2024-04-01 19:10:21 -0400

    adapter

--- MyApplication2/app/src/main/java/com/example/myapplication/Post.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Post.java
@@ -1,4 +1,43 @@
 package com.example.myapplication;
 
 public class Post {
+    private String caption,username,time,URL_content,URL_icone;
+    private Boolean isLiked;
+
+
+
+
+    public Post(String username, String time, String URL_content,String caption, String URL_icone,Boolean isLiked){
+        this.caption=caption;
+        this.isLiked=isLiked;
+        this.username=username;
+        this.URL_content=URL_content;
+        this.time=time;
+        this.URL_icone=URL_icone;
+    }
+
+    public String getUser(){
+        return username;
+    }
+
+    public String getTime(){
+        return time;
+    }
+    public String getUrl(){
+        return URL_content;
+    }
+    public String getCaption(){
+        return caption;
+    }
+
+    public String getIcone(){
+        return URL_icone;
+    }
+
+    public Boolean getLike(){
+        return isLiked;
+    }
+
+
+
 }
